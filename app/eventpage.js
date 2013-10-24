492a1849160725424a0c8b0c5578c77d7fb9e2ba
Michael Courage <michael.courage@gmail.com>
2013-10-24 00:34:57 -0700

    basically works

--- app/eventpage.js
+++ app/eventpage.js
@@ -1,5 +1,48 @@
+"use strict";
+
 console.log('hello');
 
 chrome.commands.onCommand.addListener(function(command) {
-  console.log('Command:', command);
+  commandToShortcut(command, function(shortcut) {
+    console.log('Command:', command, shortcut);
+    if (shortcut) {
+      shortcutToBookmarkNodes(shortcut, createTabsForBookmarkNodes);
+    }
+  });
 });
+
+chrome.commands.getAll(function(commands) {
+  commands.forEach(function(command) {
+    console.log(command.name, command.shortcut);
+  });
+});
+
+function commandToShortcut(commandName, callback) {
+  chrome.commands.getAll(function(commands) {
+    var notFound = commands.every(function(command) {
+      if (command.name === commandName) {
+        callback(command.shortcut);
+        return false;
+      } else {
+        return true;
+      }
+    });
+
+    if (notFound)
+      callback();
+  });
+}
+
+function shortcutToBookmarkNodes(shortcut, callback) {
+  var bracketName = '[' + shortcut + ']';
+  chrome.bookmarks.search(bracketName, callback);
+}
+
+function createTabsForBookmarkNodes(nodes) {
+  nodes.forEach(function(node) {
+    if (node.url)
+      chrome.tabs.create({ url: node.url });
+    else if (node.children)
+      createTabsForBookmarkNodes(node.children);
+  });
+}
