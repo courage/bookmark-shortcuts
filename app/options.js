6043a2bca502621cd898b73d34df0148571e3bff
Michael Courage <michael.courage@gmail.com>
2013-10-24 01:12:26 -0700

    list bookmarks from options page

--- app/options.js
+++ app/options.js
@@ -1,1 +1,11 @@
+"use strict";
 console.log('options');
+
+document.addEventListener('DOMContentLoaded', function() {
+  chrome.runtime.getBackgroundPage(function (eventpage) {
+    eventpage.getAllBookmarks(function(bookmarks) {
+      console.log(bookmarks);
+    });
+  });
+});
+
